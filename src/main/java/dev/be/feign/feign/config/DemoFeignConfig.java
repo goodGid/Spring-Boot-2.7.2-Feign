d925202e33ba00a2281cb7852734db1e1c305fef
goodGid <hello.goodgid@gmail.com>
2022-07-24 01:24:15 +0900

    Feign Interceptor 추가

--- src/main/java/dev/be/feign/feign/config/DemoFeignConfig.java
+++ src/main/java/dev/be/feign/feign/config/DemoFeignConfig.java
@@ -1,8 +1,16 @@
 package dev.be.feign.feign.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import dev.be.feign.feign.interceptor.DemoFeignInterceptor;
 
 @Configuration
 public class DemoFeignConfig {
+
+    @Bean
+    public DemoFeignInterceptor feignInterceptor() {
+        return DemoFeignInterceptor.of();
+    }
 
 }
